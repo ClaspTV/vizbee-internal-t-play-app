eea5b3a5397417d63b33572eaf1f370a612d72f9
Shiva Reddy <shiva@vizbee.tv>
2024-11-28 19:57:59 +0530

    Added app icon and launcher icon, updated logo

--- app/src/main/java/tv/vizbee/vizbeeinternaltplayapp/ui/components/TMobilePlayTopBar.kt
+++ app/src/main/java/tv/vizbee/vizbeeinternaltplayapp/ui/components/TMobilePlayTopBar.kt
@@ -1,16 +1,26 @@
 package tv.vizbee.vizbeeinternaltplayapp.ui.components
 
 import android.util.Log
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
 import tv.vizbee.tplay.CastButton
+import tv.vizbee.vizbeeinternaltplayapp.R
 
-import tv.vizbee.vizbeeinternaltplayapp.ui.theme.TMobileMagenta
 import tv.vizbee.vizbeeinternaltplayapp.ui.theme.createStyledCastButton
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -18,11 +28,15 @@ import tv.vizbee.vizbeeinternaltplayapp.ui.theme.createStyledCastButton
 fun TMobilePlayTopBar() {
     TopAppBar(
         title = {
-            Text(
-                "T MobilePlay",
-                color = TMobileMagenta,
-                fontSize = 24.sp,
-                fontWeight = FontWeight.Bold
+            Image(
+                painter = painterResource(id = R.drawable.t_mobile_play_logo),
+                contentDescription = "T-Mobile Play Logo",
+                modifier = Modifier
+                    .height(56.dp)
+                    .width(162.dp)
+                    .padding(start = 0.dp)
+                    .padding(vertical = 8.dp),
+                contentScale = ContentScale.Fit
             )
         },
 //        navigationIcon = {
