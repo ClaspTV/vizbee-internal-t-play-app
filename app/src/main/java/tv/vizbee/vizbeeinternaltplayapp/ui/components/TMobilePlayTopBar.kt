1958406eb4c6f3dea8456f30e623c0337584c696
Shiva Reddy <shiva@vizbee.tv>
2024-12-10 15:02:26 +0530

    Updated cast icons and it's supporting tplay and vizbee sdks

--- app/src/main/java/tv/vizbee/vizbeeinternaltplayapp/ui/components/TMobilePlayTopBar.kt
+++ app/src/main/java/tv/vizbee/vizbeeinternaltplayapp/ui/components/TMobilePlayTopBar.kt
@@ -1,26 +1,19 @@
 package tv.vizbee.vizbeeinternaltplayapp.ui.components
 
 import android.util.Log
-import android.view.ViewGroup
-import android.widget.ImageView
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
-import tv.vizbee.tplay.CastButton
 import tv.vizbee.vizbeeinternaltplayapp.R
-
+import tv.vizbee.vizbeeinternaltplayapp.ui.theme.TMobileMagenta
 import tv.vizbee.vizbeeinternaltplayapp.ui.theme.createStyledCastButton
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -44,11 +37,6 @@ fun TMobilePlayTopBar() {
             AndroidView(
                 factory = { context ->
                     createStyledCastButton(context)
-                },
-                update = { castButton ->
-                    // Optional: Update the cast button if needed based on state changes
-                    Log.i("VZBTPLAYAPP::CastButton", "Cast button state changed ${castButton.stateDescription}" )
-                    castButton.setDrawableTintColor("#FFCCC2DC")
                 }
             )
         }
