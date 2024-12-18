f9fce8567ba83f689400192946180ae65670d425
Shiva Reddy <shiva@vizbee.tv>
2024-12-18 10:12:29 +0530

    Updated videos

--- app/src/main/java/tv/vizbee/vizbeeinternaltplayapp/data/repository/VideoRepository.kt
+++ app/src/main/java/tv/vizbee/vizbeeinternaltplayapp/data/repository/VideoRepository.kt
@@ -31,17 +31,46 @@ class VideoRepository {
 
         // TBS Content
         Video(
+            id = "https://tbs.app.link/LwvNLbpFj8",
+            title = "Lost Resort (S1E1)",
+            subtitle = "TBS • Show",
+            thumbnailUrl = "https://images.plex.tv/photo?size=medium-360&scale=2&url=https%3A%2F%2Fimage.tmdb.org%2Ft%2Fp%2Foriginal%2FdIlcEDZNo6fBjPqbfOypCeaHd2w.jpg",
+            contentType = ContentType.TV_SHOW
+        ),
+        Video(
+            id = "https://tbs.app.link/QyCVj8czCS",
+            title = "Friends",
+            subtitle = "TBS • Show",
+            thumbnailUrl = "https://images.plex.tv/photo?size=medium-360&scale=2&url=https%3A%2F%2Fimage.tmdb.org%2Ft%2Fp%2Foriginal%2F2koX1xLkpTQM4IZebYvKysFW1Nh.jpg",
+            contentType = ContentType.TV_SHOW
+        ),
+        Video(
+            id = "https://tbs.app.link/Ln2XTeYvojb",
+            title = "Ready Player One",
+            subtitle = "TBS • Movie",
+            thumbnailUrl = "https://images.plex.tv/photo?size=medium-360&scale=2&url=https%3A%2F%2Fmetadata-static.plex.tv%2F3%2Fgracenote%2F3b12c25bdfba029dafc2fbad954f9ac5.jpg",
+            contentType = ContentType.MOVIE
+        ),
+        Video(
+            id = "https://tbs.app.link/Nlf03PMrdNb",
+            title = "The Goonies",
+            subtitle = "TBS • Movie",
+            thumbnailUrl = "https://images.plex.tv/photo?size=medium-360&scale=2&url=https%3A%2F%2Fmetadata-static.plex.tv%2Fe%2Fgracenote%2Fefbb6903d012147d22d5a1f9c701ba79.jpg",
+            contentType = ContentType.MOVIE
+        ),
+        Video(
+            id = "https://tbs.app.link/3WBd8kdi61",
+            title = "The Wizard of Oz",
+            subtitle = "TBS • Movie",
+            thumbnailUrl = "https://images.plex.tv/photo?size=medium-360&scale=2&url=https%3A%2F%2Fmetadata-static.plex.tv%2Fe%2Fgracenote%2Fefbb6903d012147d22d5a1f9c701ba79.jpg",
+            contentType = ContentType.MOVIE
+        ),
+
+        Video(
             id = "https://tbs.app.link/2qtkGJkdY0",
             title = "A Christmas Story",
             subtitle = "TBS • Movie",
             thumbnailUrl = "https://images.plex.tv/photo?size=medium-360&scale=2&url=https%3A%2F%2Fmetadata-static.plex.tv%2F3%2Fgracenote%2F3a4f1936dca41b08c0518aec1347b05b.jpg",
-            contentType = ContentType.MOVIE
-        ),
-        Video(
-            id = "https://tbs.app.link/N6kZbZtHVOb",
-            title = "Die Hard",
-            subtitle = "TBS • Movie",
-            thumbnailUrl = "https://images.plex.tv/photo?size=medium-360&scale=2&url=https%3A%2F%2Fmetadata-static.plex.tv%2F5%2Fgracenote%2F5295f9b6a1ff36985135ea39189f33f1.jpg",
             contentType = ContentType.MOVIE
         ),
         Video(
@@ -51,21 +80,21 @@ class VideoRepository {
             thumbnailUrl = "https://images.plex.tv/photo?size=medium-360&scale=2&url=https%3A%2F%2Fmetadata-static.plex.tv%2Fe%2Fgracenote%2Fea032a80c35ae7f663b60fa93e8ccdbd.jpg",
             contentType = ContentType.MOVIE
         ),
-        Video(
-            id = "https://tbs.app.link/VRLNChjknNb",
-            title = "Impractical Jokers S11 E9 One for the books",
-            subtitle = "TBS • TV Show",
-            thumbnailUrl = "https://images.plex.tv/photo?size=medium-360&scale=2&url=https%3A%2F%2Fimage.tmdb.org%2Ft%2Fp%2Foriginal%2F7ZGhfLbGUijD9OxPtBuA8xAE7Bi.jpg",
-            contentType = ContentType.TV_SHOW
-        ),
 
         // TNT Content
         Video(
-            id = "https://tnt.app.link/PV4S6SbYtN",
+            id = "https://tnt.app.link/BSEl6gDZJ5",
             title = "The Alienist S1 E1 The Boy on the Bridge",
             subtitle = "TNT • TV Show",
             thumbnailUrl = "https://images.plex.tv/photo?size=medium-360&scale=2&url=https%3A%2F%2Fimage.tmdb.org%2Ft%2Fp%2Foriginal%2F1qzuS7b2XjiVcWwmDkOyE5hIxUM.jpg",
             contentType = ContentType.TV_SHOW
-        )
+        ),
+        Video(
+            id = "https://tnt.app.link/BfSpMekyFrb",
+            title = "Creed II",
+            subtitle = "TNT • Movie",
+            thumbnailUrl = "https://images.plex.tv/photo?size=medium-360&scale=2&url=https%3A%2F%2Fmetadata-static.plex.tv%2F8%2Fgracenote%2F80c056ac7e6281be234c94abb48480db.jpg",
+            contentType = ContentType.MOVIE
+        ),
     )
 }
