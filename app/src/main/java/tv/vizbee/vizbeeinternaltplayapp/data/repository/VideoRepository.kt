1c957a0b8c8c3f2a97eb34b4d006872e4479580c
Shiva Reddy <shiva@vizbee.tv>
2024-12-19 14:24:24 +0530

    Updated videos

--- app/src/main/java/tv/vizbee/vizbeeinternaltplayapp/data/repository/VideoRepository.kt
+++ app/src/main/java/tv/vizbee/vizbeeinternaltplayapp/data/repository/VideoRepository.kt
@@ -28,6 +28,27 @@ class VideoRepository {
             thumbnailUrl = "https://images.plex.tv/photo?size=medium-360&scale=2&url=https%3A%2F%2Fmetadata-static.plex.tv%2Fc%2Fgracenote%2Fc556c4a2dc265cfa5ee451a83a34bccd.jpg",
             contentType = ContentType.MOVIE
         ),
+        Video(
+            id = "https://watch.plex.tv/show/weeds/season/5/episode/10",
+            title = "Weeds S5 E10",
+            subtitle = "Plex • Show",
+            thumbnailUrl = "https://images.plex.tv/photo?size=medium-360&scale=2&url=https%3A%2F%2Fimage.tmdb.org%2Ft%2Fp%2Foriginal%2F4tw4uoXdhmOaHSxzNHWpFv9jGvl.jpg",
+            contentType = ContentType.TV_SHOW
+        ),
+        Video(
+            id = "https://watch.plex.tv/show/weeds/season/5/episode/10",
+            title = "MythBusters S2 E7",
+            subtitle = "Plex • Show",
+            thumbnailUrl = "https://images.plex.tv/photo?size=medium-360&scale=2&url=https%3A%2F%2Fmetadata-static.plex.tv%2F8%2Fgracenote%2F8645e0ab7fea21f57afed262695bfe75.jpg",
+            contentType = ContentType.TV_SHOW
+        ),
+        Video(
+            id = "https://watch.plex.tv/show/weeds/season/5/episode/10",
+            title = "Spartacus S1 E1",
+            subtitle = "Plex • Show",
+            thumbnailUrl = "https://images.plex.tv/photo?size=medium-360&scale=2&url=https%3A%2F%2Fmetadata-static.plex.tv%2F8%2Fgracenote%2F8645e0ab7fea21f57afed262695bfe75.jpg",
+            contentType = ContentType.TV_SHOW
+        ),
 
         // TBS Content
         Video(
@@ -94,6 +115,27 @@ class VideoRepository {
             title = "Creed II",
             subtitle = "TNT • Movie",
             thumbnailUrl = "https://images.plex.tv/photo?size=medium-360&scale=2&url=https%3A%2F%2Fmetadata-static.plex.tv%2F8%2Fgracenote%2F80c056ac7e6281be234c94abb48480db.jpg",
+            contentType = ContentType.MOVIE
+        ),
+        Video(
+            id = "https://tnt.app.link/XqntkkTj4tb",
+            title = "The Lazarus Project S1 E1",
+            subtitle = "TNT • Show",
+            thumbnailUrl = "https://images.plex.tv/photo?size=medium-360&scale=2&url=https%3A%2F%2Fimage.tmdb.org%2Ft%2Fp%2Foriginal%2FilfIn5DGgZMJbJi9rwfJp3wKc0C.jpg",
+            contentType = ContentType.TV_SHOW
+        ),
+        Video(
+            id = "https://tnt.app.link/g59DjIQ3Dcb",
+            title = "Avengers: End Game",
+            subtitle = "TNT • Movie",
+            thumbnailUrl = "https://images.plex.tv/photo?size=medium-360&scale=2&url=https%3A%2F%2Fmetadata-static.plex.tv%2Fa%2Fgracenote%2Fa607d7f6f2d86d5b99b57d70e46102c1.jpg",
+            contentType = ContentType.MOVIE
+        ),
+        Video(
+            id = "https://tnt.app.link/JZTAidR0pJb",
+            title = "Die Hard",
+            subtitle = "TNT • Movie",
+            thumbnailUrl = "https://images.plex.tv/photo?size=medium-360&scale=2&url=https%3A%2F%2Fmetadata-static.plex.tv%2F5%2Fgracenote%2F5295f9b6a1ff36985135ea39189f33f1.jpg",
             contentType = ContentType.MOVIE
         ),
     )
